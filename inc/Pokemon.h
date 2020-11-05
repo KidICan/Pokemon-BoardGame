7a98e0ee12227aab297a9ba5e022fb2710f30fa4
Mario <jaimes.rene24@gmail.com>
2020-11-04 19:08:40 -0800

    Same as before

--- inc/Pokemon.h
+++ inc/Pokemon.h
@@ -23,37 +23,39 @@ public:
 typedef class Pokemon{
 
 public:
-	
-	std::string name;
-	int hp;
+    
+    std::string name;
+    int hp;
     int mhp;
     int atk;
     Move move1;
     Move move2;
-	int exp;
-	int lvl;
+    int exp;
+    int lvl;
     
-	Pokemon();
+    Pokemon();
 
-	Pokemon(std::string, int, int, int, Move, Move);
+    Pokemon(std::string, int, int, int, Move, Move);
     
     Pokemon(const Pokemon&);
     
-	virtual std:: string GetName();
+    virtual std:: string GetName();
  
-	virtual void SetName(std::string);
-	
-	virtual int GetHP();
+    virtual void SetName(std::string);
+    
+    virtual int GetHP();
     
     virtual Move getMove(int);
 
-	virtual void SetHp(int );
+    virtual void SetHp(int );
+    
+    virtual void modifyHP(int);
 
     virtual void ModifyEXP(int);
 
     virtual int GetExp();
 
-	virtual int GetLvl();
+    virtual int GetLvl();
     
     virtual void LevelUp();
     
